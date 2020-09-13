517dd9847f6e96ceeda3bd82fa1b44195e7ed676
zibig <zbiggrudzinski@gmail.com>
2020-09-13 12:19:46 +0200

    add components for recipes and shopping list

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from './../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://p0.pikist.com/photos/490/813/burger-cheese-burger-burgers-hamburger-big-burger-beef-burger-best-burger-ever-cheeseburger-huge-burger.jpg',
+    )
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
